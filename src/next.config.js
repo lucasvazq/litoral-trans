60d422eef7920f75280931fc4845e5213ea1d0fe
lucasvazq <lucas5zvazquez@gmail.com>
2021-02-19 22:09:24 -0300

    docs: add basic documentation

--- src/next.config.js
+++ src/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     env: {
         GTM: 'GTM-NGGH6LK',
         URL: 'https://https://lucasvazq.github.io/',
-        TITLE: 'LITORAL-TRANS',
-        SLOGAN: 'Empresa de transporte regional',
+        TITLE: 'Litoral Trans',
+        SLOGAN: 'Servicio de Transporte',
     },
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,96 @@
-import { NextSeo, LocalBusinessJsonLd } from 'next-seo';
+import { NextSeo } from 'next-seo';
+import {LocalBusiness, Service, WithContext} from 'schema-dts';
 
+const LB: WithContext<LocalBusiness> = {
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  "name": "Litoral Trans",
+  "image": "",
+  "@id": "https://litoral-trans.com.ar",
+  "url": "https://litoral-trans.com.ar",
+  "telephone": "",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "",
+    "addressLocality": "Rafaela",
+    "addressRegion": "AL",
+    "postalCode": "S2300",
+    "addressCountry": "US"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 40.7590403,
+    "longitude": -74.0392709
+  },
+  "openingHoursSpecification": [{
+    "@type": "OpeningHoursSpecification",
+    "dayOfWeek": "Monday",
+    "opens": "08:00",
+    "closes": "12:00"
+  },{
+    "@type": "OpeningHoursSpecification",
+    "dayOfWeek": "Monday",
+    "opens": "08:00",
+    "closes": "12:00"
+  }]
+}
+
+const Servicio: Service = {
+  // "@context": "https://schema.org/",
+  "@type": "Service",
+  "serviceType": "Servicio de Transporte",
+  "provider": {
+    "@type": "LocalBusiness",
+    "name": process.env.TITLE,
+  },
+  "areaServed": {
+    "@type": "State",
+    "name": "Área de Servicio",
+    "description": "Litoral de Santa Fe",
+  },
+  "hasOfferCatalog": {
+    "@type": "OfferCatalog",
+    "name": "Servicio de Transporte",
+    "itemListElement": [
+      {
+        "@type": "OfferCatalog",
+        "name": "House Cleaning",
+        "itemListElement": [
+          {
+            "@type": "Offer",
+            "itemOffered": {
+              "@type": "Service",
+              "name": "Apartment light cleaning"
+            }
+          },
+          {
+            "@type": "Offer",
+            "itemOffered": {
+              "@type": "Service",
+              "name": "House light cleaning up to 2 bedrooms"
+            }
+          },
+        ]
+      },
+      {
+        "@type": "OfferCatalog",
+        "name": "One-time services",
+        "itemListElement": [
+          {
+            "@type": "Offer",
+            "itemOffered": {
+              "@type": "Service",
+              "name": "Window washing"
+            }
+          },
+        ]
+      }
+    ]
+  }
+}
+
+
+// subject="Inicio"
 
 const Root = () => {
   return (
@@ -7,65 +98,12 @@ const Root = () => {
         <NextSeo
           title='Inicio'
           description={`${process.env.SLOGAN}`}
-          subject="Inicio"
           openGraph={{
             title: `${process.env.TITLE} | Inicio`,
             site_name: `${process.env.TITLE} | Inicio`,
             description: `${process.env.SLOGAN}`,
           }}
         />
-        <LocalBusinessJsonLd
-          type="Store"
-          id="http://davesdeptstore.example.com"
-          name="Dave's Department Store"
-          description="Dave's latest department store in San Jose, now open"
-          url="http://www.example.com/store-locator/sl/San-Jose-Westgate-Store/1427"
-          telephone="+14088717984"
-          address={{
-            streetAddress: '1600 Saratoga Ave',
-            addressLocality: 'San Jose',
-            addressRegion: 'CA',
-            postalCode: '95129',
-            addressCountry: 'US',
-          }}
-          geo={{
-            latitude: '37.293058',
-            longitude: '-121.988331',
-          }}
-          images={[
-            'https://example.com/photos/1x1/photo.jpg',
-            'https://example.com/photos/4x3/photo.jpg',
-            'https://example.com/photos/16x9/photo.jpg',
-          ]}
-          sameAs={[
-            'www.company-website-url1.dev',
-            'www.company-website-url2.dev',
-            'www.company-website-url3.dev',
-          ]}
-          openingHours={[
-            {
-              opens: '08:00',
-              closes: '23:59',
-              dayOfWeek: [
-                'Monday',
-                'Tuesday',
-                'Wednesday',
-                'Thursday',
-                'Friday',
-                'Saturday',
-              ],
-              validFrom: '2019-12-23',
-              validThrough: '2020-04-02',
-            },
-            {
-              opens: '14:00',
-              closes: '20:00',
-              dayOfWeek: 'Sunday',
-              validFrom: '2019-12-23',
-              validThrough: '2020-04-02',
-            },
-          ]}
-        >
         test
       </>
   );
