d667c06e064426ebf6fbd7bd7d979d90060efb1b
lucasvazq <lucas5zvazquez@gmail.com>
2021-04-18 01:15:14 -0300

    improve: improve requests connections

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,8 +13,6 @@ const App = ({ Component, props }) => (
     <Head>
       <meta charSet="utf-8" />
 
-      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet" />
-
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link rel="preconnect" href="https://www.googletagmanager.com" />
